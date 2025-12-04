e755547d9942f849b6102a8760da708297576225
Sara <sshah004@campus.goldsmiths.ac.uk>
2025-12-04 17:25:01 +0000

    weather route

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,9 @@
 const express = require("express")
 const router = express.Router()
 
+// install request
+const request = require('request');
+
 const redirectLogin = (req, res, next) => {
     if (!req.session || !req.session.userId) {
       return res.redirect("./users/login");
@@ -19,6 +22,23 @@ router.get('/about',function(req, res, next){
 });
 //just checking if this commit works
 
+//weather route
+router.get('/weather', function (req, res, next) {
+
+  let apiKey = '0fcf6ff21b366aea30bcb1cdb8bbfd2e'; // <-- put YOUR key here
+  let city = 'london';
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+  request(url, function (err, response, body) {
+      if (err) {
+          next(err);
+      } else {
+          res.send(body);
+      }
+  });
+})
+
+//logout
 router.get('/logout', redirectLogin, (req,res) => {
     req.session.destroy(err => {
     if (err) {
