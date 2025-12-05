0cb0dc3320d1fc52b11a0e4880aa901f63f15c0b
Sara <sshah004@campus.goldsmiths.ac.uk>
2025-12-05 20:00:53 +0000

    price

--- routes/api.js
+++ routes/api.js
@@ -5,22 +5,43 @@ const router = express.Router();
 router.get('/books', function (req, res, next) {
 
     let search = req.query.search; 
+    let min = req.query.minprice;
+    let max = req.query.maxprice;
 
     let sqlquery;
+    let params = [];
+    let conditions = [];
 
+    //searched something
     if (search && search.trim() !== '') {
-        search = '%' + search + '%'; 
-        sqlquery = "SELECT * FROM books WHERE name LIKE ?";
-    } else {
-        sqlquery = "SELECT * FROM books";
+        conditions.push("name LIKE ?");
+        params.push('%' + search + '%');
     }
 
-    db.query(sqlquery, search ? [search] : [], (err, result) => {
+    // min price
+    if (min && !isNaN(min)) {
+        conditions.push("price >= ?");
+        params.push(min);
+    }
+
+    // max price
+    if (max && !isNaN(max)) {
+        conditions.push("price <= ?");
+        params.push(max);
+    }
+
+    // if theres condiitions
+    if (conditions.length > 0) {
+        sqlquery += " WHERE " + conditions.join(" AND ");
+    }
+
+    db.query(sqlquery, params, (err, result) => {
         if (err) {
-            return res.json({ error: "Database error", details: err });
+            res.json({ error: err.message });
+            next(err);
+        } else {
+            res.json(result);
         }
-        //json results
-        res.json(result); 
     });
 });
 
